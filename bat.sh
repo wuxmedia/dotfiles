0f1edfc74420bbecf51ac61fb0a30b3e539d83e1
wuxmedia <wuxmedia@gmail.com>
2015-02-03 19:28:19 +0000

    Create bat.sh
    fairly light way of catching bat status.

---
+++ bat.sh
@@ -0,0 +1,25 @@
+#!/bin/bash
+
+# bat.sh
+# Queries /sys/class/power_supply/BATn/ things
+# and makes it a pretty ascii output.
+
+if [ -d /sys/class/power_supply/BAT0/ ]
+then 
+DIR=/sys/class/power_supply/BAT0/
+else
+DIR=/sys/class/power_supply/BAT1
+fi
+
+CAP=$(cat $DIR/capacity)
+STATE=$(cat $DIR/status)
+# Charging or What??
+if [[ $STATE  == D* ]]
+then ST="-"
+else ST="+"
+fi
+
+if [[ $CAP == 100 ]]
+then ST=""
+fi
+	echo {$CAP$ST]
