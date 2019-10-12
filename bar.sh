f98a977f1f06aca609817284ed24d36ff4b60c43
Tom Hardy <wuxmedia@gmail.com>
2019-10-12 14:08:23 +0100

    Update bar.sh
    added utf stuff but seems to have got borkend

--- bar.sh
+++ bar.sh
@@ -1,8 +1,10 @@
 #!/bin/sh
 # shell script to prepend i3status with more stuff
+# output_format = none in .i3status.conf
 
 i3status | while :
  do
   read line
-  echo "$(mocp -Q %title) | $(~/bin/./bat.sh)] | $line" || exit 1
+  echo "🌎 NY:$(TZ="America/New_York" date +%R) 🏠 $(/home/wux/bin/ip.sh)  $(~/bin/./bat.sh) 🌡  $(acpi -t | cut -d " " -f4| cut -d "." -f -1 )°C  $line" || exit 1
  done
+ #looks like UTFs got messed up, oh well 
